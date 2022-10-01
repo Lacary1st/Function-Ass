5be4a7ee6e4bd03afe5d116c16fbbba884db2c59
Oluwatomilola <oluwatomilola22@gmail.com>
2022-09-30 20:04:21 -1200

    done with function assignment

---
+++ script.js
@@ -0,0 +1,44 @@
+document.getElementById("topic").innerHTML="Javascript Experience.";
+document.getElementById("sub-topic").innerHTML="Javascript Function";
+document.getElementById("explanation").innerHTML="Javascript Function is a set or block of code/statement that can only be executed either by being called or invoke. Function syntax is simply written as : <code>Function   Functionname() {<code>}</code> <br> Function can be output through 4 ways, I will explain the DIRECT CALL method of executing Function, in the below illustration;";
+
+
+document.getElementById("multiply-function").innerHTML=" 1. Multiplication Function"
+
+console.log (output (20, 3))
+function output(a, b) {
+    return a * b;
+}
+
+
+document.getElementById("addition-function").innerHTML=" 2. Addition Function"
+
+console.log (sum (20, 1000))
+function sum(a, b) {
+    return a + b;
+}
+
+
+document.getElementById("subtraction-function").innerHTML=" 3. Subtraction Function"
+
+console.log (data (1984, 2022))
+function data(a, b) {
+    return a - b;
+}
+
+
+document.getElementById("division-function").innerHTML=" 4. Division Function"
+
+console.log (split (1984, 2022))
+function split (a, b) {
+    return a / b;
+}
+
+
+document.getElementById("modulus-function").innerHTML=" 5. Modulus Function";
+console.log (reminder (5, 2))
+function reminder (a, b) {
+    return a % b;
+}
+
+
